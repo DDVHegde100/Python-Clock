27c712056622b85b33a2a4713b3f2a182f8e817f
Dhruv Hegde <69629876+DDVHegde100@users.noreply.github.com>
2021-12-03 10:52:43 -0500

    Add files via upload

---
+++ main - 2021-12-03T105228.647.py
@@ -0,0 +1,14 @@
+import time
+
+def countdown(time_sec):
+    while time_sec:
+        mins, secs = divmod(time_sec, 60)
+        timeformat = '{:02d}:{:02d}'.format(mins, secs)
+        print(timeformat, end='\r')
+        time.sleep(1)
+        time_sec -= 1
+
+    print("Time is Up! Thanks and please use again 😀")
+    
+time2 = int(input("Enter a Number: "))
+countdown(time2)
